7bbb80c489a07163c1af3b8c0c4383e63ae734e8
talyt <talyta_vinente@hotmail.com>
2023-04-19 22:47:03 -0300

    Padrão de Projeto Java - Spring

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
